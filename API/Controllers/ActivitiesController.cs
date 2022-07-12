a2188bf6a205bf9cb3a7e31ab872e65ef00f841f
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-07-13 05:14:22 +0530

    adding mediator pattern and getting list of activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
-﻿using Domain;
+﻿using Application.Activities;
+using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Models;
@@ -10,21 +12,17 @@ namespace API.Controllers
 {
     public class ActivitiesController:BaseApiController
     {
-        private readonly DataContext _dataContext;
-        public ActivitiesController(DataContext dataContext)
-        {
-            _dataContext = dataContext;
 
-        }
+        
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _dataContext.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query() );
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _dataContext.Activities.FindAsync(id);
+            return  new Activity();
         }
 
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -40,6 +42,7 @@ namespace API
                     });
                 }
 			    );
+            services.AddMediatR(typeof(List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
