1333dfa868520cead4b21e20d2852cb423925862
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-07-22 23:38:27 +0530

    generic type response

--- client-app/src/App/api/agent.ts
+++ client-app/src/App/api/agent.ts
@@ -1,21 +1,22 @@
 // all req going to api
 import axios, { AxiosResponse } from "axios";
 import { ModalActions } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
 
 axios.defaults.baseURL="https:localhost:5001/api/";
-const responseBody= (response:AxiosResponse)=>response.data;
+const responseBody= <T> (response:AxiosResponse<T>)=>response.data;
  
 const requests={
-    get:(url:string)=>axios.get(url).then(responseBody),
-    post:(url:string,body:{})=>axios.post(url,body).then(responseBody),
-    del:(url:string)=>axios.delete(url).then(responseBody),
-    put:(url:string,body:{})=>axios.put(url,body).then(responseBody)     
+    get:<T>(url:string)=>axios.get<T>(url).then(responseBody),
+    post:<T>(url:string,body:{})=>axios.post<T>(url,body).then(responseBody),
+    del:<T>(url:string)=>axios.delete<T>(url).then(responseBody),
+    put:<T>(url:string,body:{})=>axios.put<T>(url,body).then(responseBody)     
 
 }
 
 const Activities=
 {
-list:()=>requests.get('Activities')
+list:()=>requests.get<IActivity[]>('Activities')
 }
 
 const agent={
