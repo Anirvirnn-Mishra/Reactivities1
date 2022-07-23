fb460a443eae885babdfc8cf1adfa34a5c9ffbee
Anirvirnn-Mishra <anirvirnmishra807@gmail.com>
2022-07-24 01:10:27 +0530

    without mobx and i will branch this

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -7,8 +7,8 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
-import { act } from 'react-dom/test-utils';
 import LoadingComponent from './LoadingComponents';
+// without mobx
 
 function App() {
 const [activities,setActivities ]=useState<IActivity[]>([]);
